29b12a6983b20d9852e6cb3697ffd3910a0f0633
Uche Madu <dreemer6@yahoo.com>
2023-09-24 11:29:26 +0100

    fix: create airflow namespace to fix dependency issues with helm release

--- terraform/k8s-secrets.tf
+++ terraform/k8s-secrets.tf
@@ -1,20 +1,29 @@
+# Airflow namespace
+resource "kubernetes_namespace" "airflow" {
+  metadata {
+    name = var.airflow_namespace
+  }
+}
+
 # Airflow webserver
 resource "kubernetes_secret" "airflow_webserver_secret" {
   metadata {
     name      = "airflow-webserver-secret"
-    namespace = var.airflow_namespace
+    namespace = kubernetes_namespace.airflow.metadata[0].name
   }
 
   data = {
     webserver-secret-key = data.google_secret_manager_secret_version.airflow_webserver_secret.secret_data
   }
+
+  depends_on = [helm_release.airflow]
 }
 
 # GitSync ssh key
 resource "kubernetes_secret" "airflow_ssh_secret" {
   metadata {
     name      = "airflow-ssh-secret"
-    namespace = var.airflow_namespace
+    namespace = kubernetes_namespace.airflow.metadata[0].name
   }
 
   data = {
