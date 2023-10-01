abd4a233cf6ccce27f92945bb6187d7e8dfe8a28
Uche Madu <dreemer6@yahoo.com>
2023-10-01 09:28:21 +0100

    fix k8s manifest resource

--- terraform/k8s-manifests.tf
+++ terraform/k8s-manifests.tf
@@ -1,5 +1,6 @@
 # ArgoCD ApplicationSet
 resource "kubernetes_manifest" "applicationset" {
+  provider   = kubernetes
   manifest   = yamldecode(file("${path.module}/../argocd-app/multi-app/applicationset.yaml"))
   depends_on = [helm_release.argocd]
 }
