8d2c4d20bc4a05ce2e5208ece87e078b6643136c
Uche Madu <dreemer6@yahoo.com>
2023-10-01 09:33:07 +0100

    fix k8s manifest resource

--- terraform/k8s-manifests.tf
+++ terraform/k8s-manifests.tf
@@ -1,6 +1,8 @@
 # ArgoCD ApplicationSet
-resource "kubernetes_manifest" "applicationset" {
-  provider   = kubernetes
-  manifest   = yamldecode(file("${path.module}/../argocd-app/multi-app/applicationset.yaml"))
-  depends_on = [helm_release.argocd]
-}
+# The cluster must already be running for this to work. Terraform apply has to be 
+# run once to create the cluster before uncommenting this  and re-running terraform apply
+# resource "kubernetes_manifest" "applicationset" {
+#   provider   = kubernetes
+#   manifest   = yamldecode(file("${path.module}/../argocd-app/multi-app/applicationset.yaml"))
+#   depends_on = [helm_release.argocd]
+# }
